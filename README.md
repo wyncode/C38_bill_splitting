8496187339ac7f620e339b202f21d688467afe9c
Leo Policastro <43428385+leopolicastro@users.noreply.github.com>
2020-07-22 08:21:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Task-Manager API
+
+
+Template repository for the Todo App code along in week 7
---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require('express'),
+  app = express();
+
+// Parse incoming JSON into objects
+app.use(express.json());
+
+module.exports = app;
