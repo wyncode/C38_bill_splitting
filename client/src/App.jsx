4ac7047d48a811e7cfd874fda1b9f842d99ad4db
cescamar <fmarq005@gmail.com>
2020-08-23 19:24:52 -0400

    inital commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { AppContextProvider } from './context/AppContext';
 import ContextDemo from './components/ContextDemo';
-
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import SignUp from './components/SignUp/SignUp';
+import Footer from './components/Footer/Footer.jsx';
 
 const App = () => {
-  const [serverMessage, setServerMessage] = useState('');
+  // const [serverMessage, setServerMessage] = useState('');
 
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
-  };
+  // const fetchDemoData = () => {
+  //   fetch('/api/demo')
+  //     .then((response) => response.json())
+  //     .then((data) => setServerMessage(data.message));
+  // };
 
-  useEffect(fetchDemoData, []);
+  // useEffect(fetchDemoData, []);
 
   return (
-    <AppContextProvider>
-      <div id="demo">
-        <h3>Hello from client/src/App.js</h3>
-        <ContextDemo />
-        <h3>{serverMessage}</h3>
-      </div>
-    </AppContextProvider>
+    <BrowserRouter>
+      <AppContextProvider>
+        <HomePage />
+        {/* <LoginPage />
+        <SignUp />
+        <ContextDemo /> */}
+        <Footer />
+      </AppContextProvider>
+    </BrowserRouter>
   );
 };
 
