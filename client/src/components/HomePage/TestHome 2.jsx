e2f5700151020a5718aed27126ac406908ccbb41
cescamar <fmarq005@gmail.com>
2020-08-26 17:31:58 -0400

    adding homepage

---
+++ client/src/components/HomePage/TestHome 2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeNavigation from '../HomeNavigation';
+
+const TestHome = () => {
+  return (
+    <div>
+      <HomeNavigation />
+    </div>
+  );
+};
+
+export default TestHome;
---
+++ client/src/components/SignUpPage/SignUpPage 2.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
+import { AppContext } from '../../context/AppContext';
+import axios from 'axios';
+
+const SignUpPage = ({ history }) => {
+  const [formData, setFormData] = useState(null);
+  const { setCurrentUser } = useContext(AppContext);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('/api/users/', formData)
+      .then((response) => {
+        sessionStorage.setItem('user', response.data);
+        setCurrentUser(response.data);
+        history.push('/');
+      })
+      .catch((error) => console.log(error, 'Sign up error'));
+  };
+
+  return (
+    <Container className="container d-flex flex-column align-items-center justify-content-center fullscreen">
+      <h1 className="mb-4 text-center">Create a CUENTA Account Now</h1>
+      <Form style={{ width: 300 }} onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label htmlFor="fullName">Full Name</Form.Label>
+          <Form.Control
+            id="fullName"
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="email">Email Address</Form.Label>
+          <Form.Control
+            id="email"
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            id="password"
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="d-flex justify-content-center">
+          <Button id="button" variant="dark" type="submit">
+            Create Account
+          </Button>
+        </Form.Group>
+      </Form>
+      <Link className="mt-2" to="/login">
+        Already a member? Login.
+      </Link>
+    </Container>
+  );
+};
+
+export default SignUpPage;
