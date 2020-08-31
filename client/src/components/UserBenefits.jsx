32045a03d289fb6415587e0738866db95290150d
Jeannie Torres <jeanniet89@gmail.com>
2020-08-31 14:19:28 -0400

    JT frontEnd edits

--- client/src/components/UserBenefits.jsx
+++ client/src/components/UserBenefits.jsx
@@ -1,37 +1,60 @@
 import React from 'react';
-import { CardColumns, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const UserBenefits = () => {
   return (
     <div>
       <h1>Cuenta user benefits</h1>
-      <CardColumns>
+      <div className="left">
+    
+      </div>
+      <div className="container">
         <Card>
-          <Card.Header>Time</Card.Header>
+        <img
+          src={'https://res.cloudinary.com/jeanniet89/image/upload/v1598822779/Cuenta%20App/Plus_blue_ohbnmw.png'}
+          alt="Time Plus Icon"
+          style={{
+            width: '20px'
+          }} /><h3 className="hometext">Time</h3> 
           <Card.Text>
             Receive daily rewards and discounts from local restaurants.
           </Card.Text>
         </Card>
         <Card>
-          <Card.Header>Rewards</Card.Header>
+        <img
+          src={'https://res.cloudinary.com/jeanniet89/image/upload/v1598822779/Cuenta%20App/Plus_blue_ohbnmw.png'}
+          alt="Time Plus Icon"
+          style={{
+            width: '20px'
+          }} /><h3 className="hometext">Rewards</h3>
           <Card.Text>
             Cuenta users save an average of 5 mins during the bill splitting
             process.
           </Card.Text>
         </Card>
         <Card>
-          <Card.Header>Transactions</Card.Header>
+        <img
+          src={'https://res.cloudinary.com/jeanniet89/image/upload/v1598822779/Cuenta%20App/Plus_blue_ohbnmw.png'}
+          alt="Plus Icon"
+          style={{
+            width: '20px'
+          }} /><h3 className="hometext">Transactions</h3>
           <Card.Text>
             Access your past transactions at any time with one click.
           </Card.Text>
         </Card>
         <Card>
-          <Card.Header>Community</Card.Header>
+        <img
+          src={'https://res.cloudinary.com/jeanniet89/image/upload/v1598822779/Cuenta%20App/Plus_blue_ohbnmw.png'}
+          alt="Plus Icon"
+          style={{
+            width: '20px'
+          }} /><h3 className="hometext">Community</h3>
           <Card.Text>
             Access your past transactions at any time with one click.
           </Card.Text>
         </Card>
-      </CardColumns>
+      </div>
     </div>
   );
 };
