a8f06afd3d8a0663ac1144bd82cb2f24baf56185
Jeannie Torres <jeanniet89@gmail.com>
2020-09-05 04:20:40 -0400

    jt-front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,23 +14,19 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="container-fluid">
-      <div className="content-wrap">
-        <AppContextProvider>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={SignUp} />
-              <Route exact path="/about" component={About} />
-              <PrivateRoute exact path="/home" component={Home} />
-              <PrivateRoute exact path="/profile" component={Profile} />
-              <PrivateRoute exact path="/billpage" component={BillPage} />
-            </Switch>
-          </BrowserRouter>
-        </AppContextProvider>
-      </div>
-    </div>
+    <AppContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/about" component={About} />
+          <PrivateRoute exact path="/home" component={Home} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+          <PrivateRoute exact path="/billpage" component={BillPage} />
+        </Switch>
+      </BrowserRouter>
+    </AppContextProvider>
   );
 };
 
