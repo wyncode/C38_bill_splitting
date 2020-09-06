e2292454ee3acbf68fe3cb76629cbe70065b5c4c
Rrudy0513 <rrudy0513@gmail.com>
2020-09-06 18:42:04 -0400

    fixed Product css

--- client/src/components/Stripe_info/Product.jsx
+++ client/src/components/Stripe_info/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import { Button, Row, Col, Container } from 'react-bootstrap';
 
 const Product = ({
   name,
@@ -9,7 +9,7 @@ const Product = ({
   isInCart
 }) => {
   return (
-    <div className="homepagetext" id="rewardsbtn">
+    <Container className="homepagetext" id="rewardsbtn">
       <Row className="product item">
         <Col>{name}</Col>
         <Col>{price}</Col>
@@ -19,7 +19,7 @@ const Product = ({
           <Button onClick={handleAddToCart}>Add to cart!</Button>
         )}
       </Row>
-    </div>
+    </Container>
   );
 };
 
