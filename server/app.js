96547852b037753ff14b351e794129e797237de5
iamsam14 <sam.robles.199@gmail.com>
2020-09-08 13:06:49 -0400

    corrected correction

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const express = require('express'),
 app.use(express.json());
 
 app.use(openRoutes);
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(userRouter);
